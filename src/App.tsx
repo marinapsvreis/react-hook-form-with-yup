b4fbf29757f35606f1bfde4a65c6e009a5018e6c
Marina Valente <marinapsvreis@gmail.com>
2024-01-11 21:52:51 -0300

    feat: validating fields and disabling save button when form is invalid

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = object({
-  name: string().required(),
-  email: string().email().required(),
-  login: string().required(),
-  password: string().required(),
-  confirmPassword: string().required(),
-  test: string(),
+  name: string().required('Required'),
+  email: string().email('Invalido email format').required('Required'),
+  login: string().required('Required'),
+  password: string().required('Required'),
+  confirmPassword: string().required('Required'),
+  test: string().required('Required'),
 });
 
 function App() {
@@ -17,11 +17,11 @@ function App() {
     control,
     handleSubmit,
     // watch,
-    // formState: { errors },
+    formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  function teste() {
-    console.log("submit");
+  function teste(data: any) {
+    console.log(data);
   }
 
   return (
@@ -35,55 +35,62 @@ function App() {
         </h1>
         <div className="flex flex-col gap-6 p-6">
           <div className="w-full">
-            <label className="text-gray-600">Name</label>
+            <label className={`text-gray-600 ${errors?.name && 'text-red-600'}`}>Name</label>
             <input
-              className="w-full px-2 py-1 rounded-md"
+              className={`w-full px-2 py-1 rounded-md ${errors?.name && 'border border-red-600'}`}
               type="text"
               {...register("name")}
             />
+            {<p className="text-red-600">{errors?.name?.message}</p>}
           </div>
           <div className="w-full">
-            <label className="text-gray-600">Email</label>
+            <label className={`text-gray-600 ${errors?.email && 'text-red-600'}`}>Email</label>
             <input
-              className="w-full px-2 py-1 rounded-md"
+              className={`w-full px-2 py-1 rounded-md ${errors?.email && 'border border-red-600'}`}
               type="text"
               {...register("email")}
             />
+            {<p className="text-red-600">{errors?.email?.message}</p>}
           </div>
           <div className="w-full">
-            <label className="text-gray-600">Login</label>
+            <label className={`text-gray-600 ${errors?.login && 'text-red-600'}`}>Login</label>
             <input
-              className="w-full px-2 py-1 rounded-md"
+              className={`w-full px-2 py-1 rounded-md ${errors?.login && 'border border-red-600'}`}
               type="text"
               {...register("login")}
             />
+            {<p className="text-red-600">{errors?.login?.message}</p>}
           </div>
           <div className="w-full">
-            <label className="text-gray-600">Password</label>
+            <label className={`text-gray-600 ${errors?.password && 'text-red-600'}`}>Password</label>
             <input
-              className="w-full px-2 py-1 rounded-md"
+              className={`w-full px-2 py-1 rounded-md ${errors?.password && 'border border-red-600'}`}
               type="password"
               {...register("password")}
             />
+            {<p className="text-red-600">{errors?.password?.message}</p>}
           </div>
           <div className="w-full">
-            <label className="text-gray-600">Confirm Password</label>
+            <label className={`text-gray-600 ${errors?.confirmPassword && 'text-red-600'}`}>Confirm Password</label>
             <input
-              className="w-full px-2 py-1 rounded-md"
+              className={`w-full px-2 py-1 rounded-md ${errors?.confirmPassword && 'border border-red-600'}`}
               type="password"
               {...register("confirmPassword")}
             />
+            {<p className="text-red-600">{errors?.confirmPassword?.message}</p>}
           </div>
           <Controller
             control={control}
             name="test"
-            render={() => (
+            render={({ field }) => (
               <div>
-                <label className="text-gray-600">Controller Test</label>
+                <label className={`text-gray-600 ${errors?.test && 'text-red-600'}`}>Controller Test</label>
                 <input
-                  className="w-full px-2 py-1 rounded-md"
+                  className={`w-full px-2 py-1 rounded-md ${errors?.test?.message && 'border border-red-600'}`} 
                   type="text"
+                  {...field}
                 />
+                {<p className="text-red-600">{errors?.test?.message}</p>}
               </div>
             )}
           />
@@ -91,8 +98,8 @@ function App() {
         <div className="flex gap-2 justify-end px-6">
           <button
             type="submit"
-            className="bg-blue-600 px-4 py-2 rounded-md text-white"
-            // disabled={true}
+            className="bg-blue-600 px-4 py-2 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={(errors?.name || errors?.email || errors?.login || errors?.password || errors?.confirmPassword || errors?.test) ? true : false}
           >
             Save
           </button>
